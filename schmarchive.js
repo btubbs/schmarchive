bdb30b0a66cf007e000623f2cf64d6c5273ad42c
Brent Tubbs <brent.tubbs@gmail.com>
2011-02-22 03:54:52 -0500

    starting to get slidey

--- schmarchive.js
+++ schmarchive.js
@@ -3,6 +3,17 @@ var PERM_COPY = 0x00008000;
 var PERM_MODIFY = 0x00004000;
 var PERM_TRANSFER = 0x00002000;
 
+//make a fix for console.log so it doesn't crash the script in browsers that
+//don't have it.
+if (!window.hasOwnProperty('console')) {
+    window.console = {};
+    window.console.log = function(msg) {
+        //do nothing
+    }
+}
+
+console.log('testing');
+
 //make a namespace
 if (typeof Schmo === 'undefined') {
     var Schmo = {};
@@ -37,7 +48,8 @@ Schmo.getURL = function(path) {
 //html file
 
 Schmo.inventory = {
-    appendTemplateItems: function(container, items, template) {
+    fillContents: function(container, items) {
+        var template = '#item_template';
         for (i in items) {
             var item = items[i];
             //make a user friendly string out of the permissions mask.
@@ -58,17 +70,48 @@ Schmo.inventory = {
         }
     },
 
-    fillContents: function(container, items) {
-        Schmo.inventory.appendTemplateItems(container, items, '#item_template');
-        //now add each item's whole info object as data
-        //for (i in items) {
-
-        //}
+    bindSlider: function() {
+        $('.inv_child').click(function() {
+            //create 'new' inv box
+            var info = $(this).data('info');
+            info.classes = 'new';
+            $('#box_template').tmpl(info).appendTo('#inv_container');
+            //slide over
+            $('.inv_block.shown').addClass('dying');
+            $('.inv_block.shown').animate({
+                left: '-305px'
+            }, function() {
+                $('.inv_block.dying').remove();
+                $('.inv_block.new').addClass('shown').removeClass('new');
+                Schmo.inventory.bindSlider();
+            });
+            $('.inv_block.new').animate({left: '5px'});
+        });
     },
 
-    //fillChildren: function(container, children) {
-        //Schmo.inventory.appendTemplateItems(container, children, '#child_template');
-    //},
+    fillChildren: function(container, children) {
+
+        //sort the children in order of obj name
+        children.sort(function(a, b) {
+            a = a.objname.toLowerCase();
+            b = b.objname.toLowerCase();
+            if (a < b) {
+                return -1;
+            } else if (a > b) {
+                return 1;
+            } else {
+                return 0;
+            }
+        });
+        var template = '#child_template';
+        for (i in children) {
+            var child = children[i];
+            child.link = '?url=' + child.url + '&userkey=' + Schmo.qParam('userkey');
+            $(template).tmpl(child).appendTo(container);
+            $('#' + child.objkey).data('info', child);            
+        }
+        Schmo.inventory.bindSlider();
+    },
 }
 
 var highlightItem = function(item) {
@@ -83,19 +126,28 @@ $(document).ready(function() {
     //TODO: show children 
     //show info
     var info_url = Schmo.getURL('/info/');
-    console.log('requesting info');
     $.getJSON(info_url, function(obj_data) {
+        console.log(obj_data);
         //personalize
         var rendered = $('#av_template').tmpl(obj_data);
         $('#av_container').html(rendered);
         //create a new box for this inv box
+        obj_data.classes = 'shown';
         $('#box_template').tmpl(obj_data).appendTo('#inv_container');
         var new_box = $('#' + obj_data.objkey);
         //stash some data in the new box
         $(new_box).data('box_url', Schmo.getURL('/give/'));
+
+        //show_children
+        var children_url = Schmo.getURL('/children/');
+        $.getJSON(children_url, function(data) {
+            console.log('children');
+            console.log(data);
+            Schmo.inventory.fillChildren(new_box, data);
+        });
+
         //show inventory
         var inventory_url = Schmo.getURL('/inv/'); 
-        //make a jsonp call back to the box and put the response in the #item_list
         $.getJSON(inventory_url, function(data) {
             Schmo.inventory.fillContents(new_box, data);
             //bind a function to each item in the box that will make it get delivered on touch
